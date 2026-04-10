066e70e46618a05cef7e511a20f3b8cbb0fc4ae6
Fil Filimonov <flippfil11@gmail.com>
2026-04-10 10:25:41 +0300

    create auth service

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,15 +1,58 @@
+from backend.database.user_repo import UserRepository
+from backend.database.session_repo import SessionRepository
+from backend.core.security import PasswordHasher
+from backend.entities.user import User
+from backend.entities.session import Session
 
+class AuthError (Exception):
+    pass
+
+class PasswordError(Exception):
+    pass
 
 class AuthorizationService:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, user_repo: UserRepository, session_repo: SessionRepository, hasher: PasswordHasher) -> None:
+        self._user_repo = user_repo
+        self._session_repo = session_repo
+        self._hasher = hasher
     
-    def register(self):
-        pass
+    def register(self, username, password) -> dict:
+        username = username.strip()
+        if self._user_repo.exists_user(username):
+            raise AuthError(f"User {username}, уже есть [REGISTER]")
+        
+        if not self._hasher.is_strong(password):
+            raise PasswordError("не надежный пароль [REGISTER]")
+        
+        hash_password = self._hasher.hash(password)
+        
+        user = User(username, hash_password)
+        
+        self._user_repo.create_user(user)
+        session = self._session_repo.create_session(user.id)
+        
+        return {"token": session.token, "user": user.to_dict()}
     
-    def login(self):
-        pass
+    def login(self, username, password) -> dict:
+        username = username.strip()
+        if not self._user_repo.exists_user(username):
+            raise AuthError(f'User {username} не существует [LOGIN]')
+        user = self._user_repo.get_user_by_username(username)
+        self._hasher.verify(password, self._hasher.hash(password))
+        session = self._session_repo.create_session(user.id)
+        
+        return {"token": session.token, "user":user.to_dict()}
     
-    def get_user(self):
-        pass
+    def get_current_user(self, token) -> User:
+        session = self._session_repo.get_by_token(token)
+        if session == None:
+            raise AuthError("not found by token [GET CURRENT USER]")
+        return self._user_repo.get_user_by_id(session.user_id)
+    
+    def logout(self, token) -> None:
+        if self._session_repo.get_by_token(token) == None:
+            raise AuthError(f"[LOGOUT] not found by token")
+        self._session_repo.delete_by_token(token)
+        
+        
